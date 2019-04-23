97d542be8d1058fbf7ac9e40d8e33daedd451474
VladislaS <shorsavlad@gmail.com>
2019-04-23 19:06:28 +0300

    FE12

--- Homework_12/homework_12.1.js
+++ Homework_12/homework_12.1.js
@@ -1,24 +1,32 @@
+
+// Захостить на гитхабе приложение, которое:
+// читает куки на клиенте, и если там есть дата последнего посещения, выводит ее на страницу
+// пишет в куки на клиенте текущую дату посещения
+
 let practiceGetSet = {
-    cookies: window.document.cookie,
-    res: {},
+	cookies : window.document.cookie,
+	res : {},
+	
+	get getCookies(){
+		this.cookies.split('; ').map(
+			item => { return {[item.split('=')[0]] : item.split('=')[1]} }
+		).forEach(
+			item => Object.assign(this.res, item)
+ 		)
+		if( 'date' in this.res){
 
-    get getCookies() {
-        this.cookies.split('; ').map(
-            item => {
-                return {[item.split('=')[0]]: item.split('=')[1]}
-            }
-        ).forEach(
-            item => Object.assign(this.res, item)
-        );
-        document.body.appendChild(document.createElement('div'))
-            .textContent = JSON.stringify(this.res)
-    },
-    set addCookies(obj) {
-        let arrCoocie = Object.entries(obj);
-        let arr = [];
-        arrCoocie.forEach(item => arr.push(`${item[0]}=${item[1]}; `));
-        document.cookie = arr.join('')
-    }
-};
+		document.body.appendChild(document.createElement('div'))
+			.textContent = JSON.stringify(this.res)
+		} 
+		this.addCookies = {date : new Date().toLocaleDateString() }
 
-document.cookie ? practiceGetSet.getCookies : practiceGetSet.addCookies = {date: new Date().toLocaleDateString()};
+	},
+	set addCookies(obj){
+		let arrCoocie = Object.entries(obj)
+		let arr =[];
+		arrCoocie.forEach(item => arr.push(`${item[0]}=${item[1]}; `))
+		document.cookie = arr.join('')
+	}
+}
+
+document.cookie ? practiceGetSet.getCookies : practiceGetSet.addCookies = {date : new Date().toLocaleDateString() } ;
